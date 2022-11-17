2dd98286883b350139ed470fba5937635ebff7f5
bre-nda <brenda.kanale@student.moringaschool.com>
2022-11-17 23:09:24 +0300

    deploy app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl : 'http://localhost:3000/Wallet_Balances',
+  contUrl : 'http://localhost:3000/gifts',
+  redUrl : 'http://localhost:3000/gift_redemptions',
+  buyUrl : 'http://localhost:3000/gift_accounts',
+  messUrl : 'http://localhost:3000/outgoing_msg',
+  usersUrl : 'http://localhost:3000/users',
+  sessUrl : 'http://localhost:3000/sessions',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl : 'http://localhost:3000/Wallet_Balances',
+  contUrl : 'http://localhost:3000/gifts',
+  redUrl : 'http://localhost:3000/gift_redemptions',
+  buyUrl : 'http://localhost:3000/gift_accounts',
+  messUrl : 'http://localhost:3000/outgoing_msg',
+  usersUrl : 'http://localhost:3000/users',
+  sessUrl : 'http://localhost:3000/sessions',
 };
 
 /*
